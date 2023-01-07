c32792490828ef215b23ae5201fc967d5f7ac7aa
Naoya Teraoka <59727299+Naoya323150403@users.noreply.github.com>
2023-01-08 01:03:08 +0900

    2023/01/07

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
+import { Todo } from "./Todo";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
 
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ受信</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userId} />
+      ))}
     </div>
   );
 }
